baba84f392a3d5fb8c965b64ffb51f9877475efd
coderdan1399 <coderdan1399@gmail.com>
2018-06-04 21:28:30 +1200

    ngrx freeze, router and homepage component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,15 +6,33 @@ import { StoreModule } from '@ngrx/store';
 import { reducers, metaReducers } from './reducers';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { environment } from '../environments/environment';
+import { AppRoutingModule } from './/app-routing.module';
+import { HomePageComponent } from './home-page/home-page.component';
+import {
+  StoreRouterConnectingModule,
+  RouterStateSerializer,
+} from '@ngrx/router-store';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent
   ],
   imports: [
     BrowserModule,
     StoreModule.forRoot(reducers, { metaReducers }),
-    !environment.production ? StoreDevtoolsModule.instrument() : []
+    !environment.production ? StoreDevtoolsModule.instrument() : [],
+    /**
+ * @ngrx/router-store keeps router state up-to-date in the store.
+ */
+    StoreRouterConnectingModule.forRoot({
+      /*
+        They stateKey defines the name of the state used by the router-store reducer.
+        This matches the key defined in the map of reducers
+      */
+      stateKey: 'router',
+    }),
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/reducers/index.ts
+++ src/app/reducers/index.ts
@@ -6,14 +6,25 @@ import {
   MetaReducer
 } from '@ngrx/store';
 import { environment } from '../../environments/environment';
-
+import * as fromRouter from '@ngrx/router-store';
+import { storeFreeze } from 'ngrx-store-freeze';
 export interface State {
 
 }
 
 export const reducers: ActionReducerMap<State> = {
-
+  router: fromRouter.routerReducer,
 };
 
 
-export const metaReducers: MetaReducer<State>[] = !environment.production ? [] : [];
+export const metaReducers: MetaReducer<State>[] = !environment.production ? [] : [logger, storeFreeze];
+
+
+export function logger(reducer: ActionReducer<State>): ActionReducer<State> {
+  return function (state: State, action: any): State {
+    console.log('state', state);
+    console.log('action', action);
+
+    return reducer(state, action);
+  };
+}
