7fa23327d67d711a07e1f8840553b66555f4c225
coderdan1399 <coderdan1399@gmail.com>
2018-06-14 00:39:37 +1200

    add user url

--- src/app/components/user-list/user-list.component.ts
+++ src/app/components/user-list/user-list.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { User } from '../../redux/models/user.model';
 import { Input } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'user-list',
@@ -8,4 +10,20 @@ import { Input } from '@angular/core';
 })
 export class UserListComponent {
   @Input() public users: User[];
+  constructor(private location: Location) {}
+
+  public getUserUrl(user: User) {
+    const url = this.location.prepareExternalUrl(
+      `/team/${user.teamId}/coffee?user=${user.id}&secret=${user.secret}`
+    );
+    return `${window.location.protocol}//${window.location.hostname}:${
+      window.location.port
+    }${url}`;
+    // .createUrlTree([':/team', user.teamId, 'user', user.secret])
+    // .toString();
+  }
+
+  public selectAll(event) {
+    event.srcElement.select();
+  }
 }
